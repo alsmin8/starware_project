8580285af1663ba1eb6a993214c82185af3dc155
이빛 <@LAPTOP-FSEBFTIK>
2019-02-12 09:23:39 +0900

    test12

--- Team_Project/src/main/java/kosta/starware/controller/ApprovalController.java
+++ Team_Project/src/main/java/kosta/starware/controller/ApprovalController.java
@@ -4,8 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import kosta.starware.domain.Approval;
 import kosta.starware.service.ApprovalService;
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
@@ -22,5 +26,37 @@ public class ApprovalController {
 		log.info("list.........................."+ approvalservice.appList());
 		model.addAttribute("list", approvalservice.appList());
 	}
-
+	
+	@PostMapping("/appinsertddform")
+	public String appInsert(Approval approval, RedirectAttributes rttr){
+		log.info("appInsert::"+approval);
+		approvalservice.appInsert(approval);
+		rttr.addFlashAttribute("result", approval.getApp_no());
+		return "redirect:/approval/applist_alllist";
+	}
+	
+	@GetMapping("/appdetaildd")
+	public void appDetail(@RequestParam("app_no") int app_no, Model model){
+		log.info("/appdetaildd");
+		model.addAttribute("approval", approvalservice.appDetail(app_no));
+	}
+	
+	@PostMapping("/appupdateddform")
+	public String appUpdate(Approval approval, RedirectAttributes rttr){
+		log.info("update........"+approval);
+		
+		if(approvalservice.appUpdate(approval)){
+			rttr.addFlashAttribute("result","success");
+		}
+		return "redirect:/approval/applist_alllist";
+		}
+	@PostMapping("/delete")//????????????????????????????
+	public String appDelete(@RequestParam("app_no") int app_no, RedirectAttributes rttr){
+		log.info("delete////////"+app_no);
+		if(approvalservice.appDelete(app_no)){
+			rttr.addFlashAttribute("result","success");
+		}
+		return "redirect:/approval/applist_alllist";
+	}
 }
+
--- Team_Project/src/test/java/kosta/starware/controller/ApprovalControllerTest.java
+++ Team_Project/src/test/java/kosta/starware/controller/ApprovalControllerTest.java
@@ -1,13 +1,15 @@
 package kosta.starware.controller;
 
 import org.aspectj.lang.annotation.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import lombok.Setter;
@@ -21,13 +23,20 @@ import lombok.extern.log4j.Log4j;
 })
 @Log4j
 public class ApprovalControllerTest {
-	@Setter(onMethod_ = {@Autowired})
+	@Setter(onMethod_ = {@Autowired} )
 	private WebApplicationContext ctx;
 	
 	private MockMvc mockMvc;
 	
-	/*@Before
+	@org.junit.Before
 	public void setup(){
-		this.mockMvc = MockMvcBuilders.
-	}*/
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(ctx).build();
+	}
+	@Test
+	public void testList() throws Exception{
+	log.info(mockMvc.perform(MockMvcRequestBuilders.get("/approval/applist_alllist"))
+			.andReturn()
+			.getModelAndView()
+			.getModelMap());
+}
 }
