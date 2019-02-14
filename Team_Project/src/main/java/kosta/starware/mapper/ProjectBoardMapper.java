f6112101663ffd23ce3a639c30b94823471c7a6b
MinGyu <MinGyu@DESKTOP-IOOQ2FU>
2019-02-14 09:48:09 +0900

    김민규 14일 프로젝트 게시판 조금...

---
+++ Team_Project/src/main/java/kosta/starware/mapper/ProjectBoardMapper.java
@@ -0,0 +1,21 @@
+package kosta.starware.mapper;
+
+import java.util.List;
+
+import kosta.starware.domain.ProjectBoardVO;
+
+public interface ProjectBoardMapper {
+	
+	public int insert(ProjectBoardVO vo);
+	
+	public List<ProjectBoardVO> list(int project_No);
+	
+	public ProjectBoardVO read(int project_Board_No);
+	
+	public int delete(int project_Board_No);
+	
+	public int update(ProjectBoardVO board);
+	
+	//public int move()
+
+}
---
+++ Team_Project/src/main/java/kosta/starware/service/ProjectBoardServiceImpl.java
@@ -0,0 +1,62 @@
+package kosta.starware.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import kosta.starware.domain.ProjectBoardVO;
+import kosta.starware.mapper.ProjectBoardMapper;
+import lombok.extern.log4j.Log4j;
+
+@Service
+@Log4j
+public class ProjectBoardServiceImpl implements ProjectBoardService {
+
+	private ProjectBoardMapper mapper;
+	
+	@Override
+	public int register(ProjectBoardVO vo) {
+		
+		log.info("register.........." + vo);
+		
+		return mapper.insert(vo);
+	}
+
+	
+
+	@Override
+	public List<ProjectBoardVO> getList(int project_No) {
+		
+		log.info("getList............" + project_No);
+		
+		return mapper.list(project_No);
+		
+	}
+
+
+
+	@Override
+	public ProjectBoardVO get(int project_Board_No) {
+		
+		log.info("get................" + project_Board_No);
+		
+		return mapper.read(project_Board_No);
+	}
+
+	@Override
+	public int remove(int project_Board_No) {
+		
+		log.info("remove............." + project_Board_No);
+		
+		return mapper.delete(project_Board_No);
+	}
+
+	@Override
+	public int modify(ProjectBoardVO board) {
+		
+		log.info("modify..........." + board);
+		
+		return mapper.update(board);
+	}
+
+}
